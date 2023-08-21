5d5768e56c6d42f575668d62f56a748033d54c0f
Kareem1715 <Kareem.H1715@gmail.com>
2023-08-21 22:20:27 +0300

    Advanced 1

--- main.h
+++ main.h
@@ -1,15 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 #include <limits.h>
 #include <stdarg.h>
 #include "string.h"
 #include <stdlib.h>
-
 int _printf(const char *format, ...);
+int format_char(va_list ap);
+int format_string(va_list ap);
+int rec_num(int num);
 int print_numbers(int n, int *len);
-<<<<<<< HEAD
 int print_binary(int number, int *length);
 
 /**
@@ -24,6 +24,4 @@ typedef struct list
 	char *s;
 	int (*f)(va_list);
 } List;
-=======
->>>>>>> ede08e65f8868c3958be1a0bb52b62c77855bbf0
 #endif
